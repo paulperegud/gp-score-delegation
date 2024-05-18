2ca53b95787cf38225b90d1312eeb0f7fc06b266
Pawel Peregud <paulperegud@protonmail.com>
2024-05-18 15:35:39 +0200

    message.py create Prover.toml

--- message.py
+++ message.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i python3 -p python3Packages.web3 python3Packages.setuptools python3Packages.click
+#!nix-shell -i python3 -p python3Packages.web3 python3Packages.setuptools python3Packages.click python3Packages.toml
 
 from web3 import Web3
 from eth_account.messages import encode_defunct
 from eth_keys import keys
+import toml
 
 w3 = Web3()
 
 def arr(b):
-    return [ x for x in b ]
+    return [ str(x) for x in b ]
 
 msg = "delegate"
 
@@ -17,12 +18,15 @@ pk = keys.PrivateKey(acc.key)
 
 message = encode_defunct(text=msg)
 signed_message = w3.eth.account.sign_message(message, private_key=acc.key)
-print(f"message: {msg}")
-print(f"privkey: {acc.privateKey.hex()}")
-print(f"pubkey: {pk.public_key}")
-print(f"pubkey x: {arr(pk.public_key[:32])}")
-print(f"pubkey y: {arr(pk.public_key[32:])}")
-print(f"address: {acc.address}")
-print(f"msg hash: {arr(signed_message.messageHash)}")
-print(f"signature: {arr(signed_message.signature)}")
-print(f"nullifier web3: {arr(Web3.keccak(signed_message.signature[:64]))}")
+
+prover = dict()
+preimage = dict()
+preimage['pub_key_x'] = arr(pk.public_key[:32])
+preimage['pub_key_y'] = arr(pk.public_key[32:])
+preimage['signature'] = arr(signed_message.signature[:64])
+prover['preimage'] = preimage
+prover['hashed_message'] = arr(signed_message.messageHash)
+prover['nullifier'] = arr(Web3.keccak(signed_message.signature[:64]))
+
+with open('Prover.toml', 'w', encoding='utf8') as f: 
+    f.write(toml.dumps(prover))
